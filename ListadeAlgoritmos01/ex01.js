3fc0e2bca274bc381f376bf46bc35d4f9efdceee
Adriano Sato <97803243+adriano-sato@users.noreply.github.com>
2022-05-07 12:14:33 -0300

    Primeiro algoritmo

---
+++ ListadeAlgoritmos01/ex01.js
@@ -0,0 +1,9 @@
+const nome = "Adriano";
+const idade = 25;
+
+if (idade >= 18){
+    console.log(`${nome} tem ${idade} anos e é maior de idade.`);
+}else{
+        console.log(`${nome} tem ${idade} anos e é menor de idade.`);
+};
+
